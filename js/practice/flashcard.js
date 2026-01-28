949ecab6dff9cb629e40725ca2cafbf089961c9f
BaeHannah <156322702+HannahWaan@users.noreply.github.com>
2026-01-28 15:56:44 +0700

    Update flashcard.js

--- js/practice/flashcard.js
+++ js/practice/flashcard.js
@@ -47,7 +47,11 @@ export function startFlashcard(scope, settings = {}) {
     }
     
     showPracticeArea();
+    console.log('[flashcard] rendering into #practice-content', !!document.getElementById('practice-content'));
+  renderFlashcardUI();
+  console.log('[flashcard] rendered len=', document.getElementById('practice-content')?.innerHTML?.length);
 
+  
     autoPlayEnabled = mergedSettings.autoPlay;
     
     renderFlashcardUI();
@@ -402,6 +406,7 @@ window.speakCurrentFlashcard = speakCurrentFlashcard;
 window.exitFlashcard = exitFlashcard;
 window.restartFlashcard = restartFlashcard;
 window.reviewWrongFlashcards = reviewWrongFlashcards;
+
 
 
 
